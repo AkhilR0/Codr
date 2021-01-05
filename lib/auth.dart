76aca5e19ef76bd398723e43c279c4cb27116986
AkhilR0 <akhilramidi0@gmail.com>
2021-01-04 18:36:27 -0600

    test

--- lib/auth.dart
+++ lib/auth.dart
@@ -3,7 +3,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
-class AuthService{
-
-  
+class AuthService {
+  String name;
 }
